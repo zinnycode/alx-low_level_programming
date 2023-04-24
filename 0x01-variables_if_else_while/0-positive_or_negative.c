fce97b30cc23f42d7c805d089ab05e047a2cca8b
zinnycode <zinnygold01@gmail.com>
2023-04-24 11:29:49 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,17 @@ int main(void)
 
     srand(time(0));
     n = rand() - Rand_MAX / 2;
-     
     if (n > 0);
     {
-            printf("%d is positive\n", n);
+      printf("%d is positive\n", n);
     }
     else if (n == 0);
     {
-	    printf("%d is zero\n", n)' 
-    }              
+     printf("%d is zero\n", n);
+    }
     else
     {
 	    printf("%d is negative\n", n);
     }
-    return (0);	    
+    return (0);
 }
