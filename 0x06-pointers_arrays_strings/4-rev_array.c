42a59a116a2f931ff270590738267cbbaf9b2a45
zinnycode <zinnygold01@gmail.com>
2023-05-04 11:02:04 -0700

    4-rev_array.c unfinished

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array
+ * @n: number of elements of the array
+ * Return: rev
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n
